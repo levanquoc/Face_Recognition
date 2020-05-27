11ab960120daed5a3fc94a5d5ee7717f2bfb0fb6
levanquoc <levanquocbkdn@gmail.com>
2020-05-27 06:04:37 +0000

    changes mask_images

--- mask_images.py
+++ mask_images.py
@@ -2,14 +2,24 @@ import cv2
 import os
 from mask import create_mask
 
+from imutils import paths
+path="./dataset_maskedface"
+folder=(os.listdir("dataset"))
 
-folder_path = "./dataset"
-#dist_path = "/home/preeth/Downloads"
+for i,namefolder in enumerate(folder):
+    if namefolder in os.listdir(path):
+        pass
+    else:    
+        try:
+            os.mkdir(os.path.join(path,namefolder))
+        except Exception as e:
+            print(e)
 
 #c = 0
-images = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+images = list(paths.list_images("dataset"))
 for i in range(len(images)):
     print("the path of the image is", images[i])
     #image = cv2.imread(images[i])
     #c = c + 1
     create_mask(images[i])
+    
